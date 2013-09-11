86f0a858d631e82e27908c2423458e4a8ff5b503
Fedir Panasenko <fpanasenko@gmail.com>
2013-09-11 15:02:44 +0300

    removed metadata from comments

---
+++ post-meta-page.php
@@ -0,0 +1,17 @@
+<?php
+
+// Exit if accessed directly
+if ( !defined('ABSPATH')) exit;
+
+/**
+ * Page Meta-Data Template-Part File
+ *
+ * @file           post-meta-page.php
+ * @package        AI
+ * @author         Emil Uzelac, Fedir Panasenko
+ * @license        license.txt
+ * @filesource     wp-content/themes/ai/post-meta-page.php
+ */
+?>
+
+<h1 class="entry-title post-title"><?php the_title(); ?></h1>
